3d240f454bb1dfff0659c8dbd92c0763e07d24b9
FigueiredoBarros <38756019+Lops5gx@users.noreply.github.com>
2019-05-29 20:29:30 -0300

    Update Teste.java

--- Projeto/src/Teste.java
+++ Projeto/src/Teste.java
@@ -20,7 +20,7 @@ public class Teste {
 		System.out.println("A soma:      " + somaConjunto(A,B, 8));
 		
 		
-//		combina(a, 0, new ArrayList<>());
+
 		
 		/*
 		String U = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,{1,2}}";
@@ -51,64 +51,34 @@ public class Teste {
 	}
 	
 	//sandro.j@pucminas.br
-//	public static void print(ArrayList<Integer> solucao) {
-//		for(int i=0;i<solucao.size();i++) {
-//			
-//			System.out.print(solucao.get(i) + " - ");
-//		}
-//		System.out.print("\n");
-//	}
-//	
-//	public static void combina(int []conjunto, int atual, ArrayList<Integer> solucao) {
-//		
-//		print(solucao);
-//		for(int i=0;i<conjunto.length;i++) {
-//			if (atual != i) {
-//				solucao.add(conjunto[i]);
-//				combina(conjunto, i, solucao);
-//				solucao.remove(solucao.size()-1)
-//;			}
-//		}
-//		
-//	}
-//	
-//	public static void partes(Conjunto a) {
-//		
-//		 String linha="P(" + a.getNome() + ") = {";
-//		 
-//		 for(int i=0;i<(1<<a.getCardinalidade());i++) {
-//
-//		  linha+=" { ";
-//		  
-//		  for(int j=0; j<a.getCardinalidade();j++) {
-//		   if((i & (1<<j))!=0) {
-//		    linha+=a.getElemento(j)+ ",";
-//		    
-//		   }
-//		  }
-//		  linha+=" } ,";
-//		 }
-//		 linha+=" }";
-//		 linha = linha.replace(",}", "}").replace(", }", "}");
-//		 System.out.println(linha); 
-//		}
 	
 	
-//	public static void partes(Conjunto a){
-//		
-//		 String linha = "P(" + a.getNome() + ") = {";
-//		 
-//		 for(int i=0; i< a.getCardinalidade(); i++) {
-//			 
-//			 linha+=" { ";
-//			 
-//			 for(int j=0; j< a.getCardinalidade(); j++) {
-//				 if( j  +  1/ j+1 != 0 ) {
-//					 System.out.println("Entrei aqui dentro");
-//				 }
-//			 }
-//		 }
-//	}
+	
+	//METODO DO CONJUNTO DAS PARTES
+	public static Conjunto partes(Conjunto a) {
+		
+		 String linha="P(" + a.getNome() + ") = {";
+		 
+		 for(int i=0;i<(1<<a.getCardinalidade());i++) {
+
+		  linha+=" { ";
+		  
+		  for(int j=0; j<a.getCardinalidade();j++) {
+		   if((i & (1<<j))!=0) {
+		    linha+=a.getElemento(j)+ ",";
+		    
+		   }
+		  }
+		  linha+=" } ,";
+		 }
+		 linha+=" }";
+		 linha = linha.replace(",}", "}").replace(", }", "}");
+		 Conjunto C = new Conjunto(linha);
+		 return C; 
+		}
+	
+	
+
 
 	
 	
@@ -116,68 +86,14 @@ public class Teste {
 	
 	
 	
-//	public static void conjuntoDasPartes(Conjunto A) {
-//		System.out.println();
-//		int i=0;
-//		String linhafinal = "}";
-//		String linhainicio = "{ Ø"; 
-//		String linhaAux = "{";
-//		
-//		String linha1  = "";
-//		String linha2  ="";
-//		String linha3  = "";
-//		
-//
-//		
-//			
-//		linha1 += linhainicio + ",{";
-//		linha2 += linhaAux;
-//		linha3 += linhaAux; 
-//		
-//	
-//			while(i < A.getCardinalidade()) {
-//				
-//				
-//				linha1 += A.getElemento(i) + "},";
-//				linha1 += linhaAux;
-//				
-//				linha2 += A.getElemento(i) + ",";
-//				if(i == 0) {
-//					linha3 +=  A.getElemento(i) + "," + A.getElemento(i + 1) + linhafinal;
-//				}
-//				else if(i == 1) {
-//					linha3 += linhaAux + A.getElemento(i-1) + "," + A.getElemento(i+1) + linhafinal +",";
-//				}else if(i == 2) {
-//					System.out.println("VALOR DO I: " + i);
-//					i=i-1;
-//					linha3 += linhaAux + A.getElemento(i) + "," + A.getElemento(i+1) + linhafinal;	
-//					i=i+1;
-//				}else if(i == 3) { }
-//				
-//				i++;
-//			}
-//			linha1 += linhafinal;
-//			linha2 += linhafinal + ",";
-//			linha3 += linhafinal;
-//			
-//			System.out.println("linha1" + linha1);
-//			System.out.println("linha2" + linha2);
-//			System.out.println("linha3" + linha3);
-//			
-//			String Concatena = linha1 + linha2 + linha3;
-//			System.out.println();
-//			System.out.println();
-//			System.out.println();
-//			System.out.println("Conjunto das Partes: "+ Concatena);
-//
-//		}
+
 	
 	
 	
 	
 	
 	
-	
+	// MÉTODO DE SOMA
 	public static Conjunto  somaConjunto(Conjunto A, Conjunto B, int parametro) {
 		int somaAB = 0;
 		String concatena = "";
